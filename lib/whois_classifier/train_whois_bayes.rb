c66715081b91e32140bb1eac9898797adda2292e
Steeve McCauley <steeve@oneguycoding.com>
2016-11-01 09:20:57 -0400

    * added :ignore category for whois fields to ignore * testing some training, and discovery approaches

--- lib/whois_classifier/train_whois_bayes.rb
+++ lib/whois_classifier/train_whois_bayes.rb
@@ -2,27 +2,40 @@
 #
 
 require 'classifier-reborn'
+require 'json'
 
-#CAT = %w/abuse-c abuse-mailbox address admin-c country created descr fax-no inetnum last-modified mnt-by mnt-ref netname nic-hdl org organisation org-name org-type origin phone remarks role route source status tech-c/
+#$cat = %w/abuse-c abuse-mailbox address admin-c country created descr fax-no inetnum last-modified mnt-by mnt-ref netname nic-hdl org organisation org-name org-type origin phone remarks role route source status tech-c/
 
-CAT = {
+$cat = {
 	:netrange => %w/netrange inetnum/,
 	:cidr     => %w/cidr route/,
 	:country  => %w/country/,
 	:regdate  => %w/regdate created/,
-	:updated  => %w/updated last-modified/
+	:updated  => %w/updated last-modified/,
+	:ignore   => %w//
 }
-IGNORE = {
+$ignore = %w/abuse-c abuse-mailbox address phone fax-no org organisation org-name org-type netname status origin remarks admin-c tech-c mnt-ref mnt-by/
+$ignore.concat(%w/descr source role nic-hdl mnt-routes mnt-domains person at https via nethandle parent nettype originas customer ref custname city stateprov postalcode orgtechhandle orgtechname orgtechphone orgtechemail orgtechref orgabusehandle orgabusename orgabusephone orgabuseemail orgabuseref rtechhandle rtechname rtechphone rtechemail rtechref organization orgname orgid comment/)
 
-}
-
-wbc = ClassifierReborn::Bayes.new CAT.keys
+wbc = ClassifierReborn::Bayes.new $cat.keys
 
 puts wbc.categories
 
 RE_COMMENT=/(.*)(%.*)$/
 RE_CAT=/([-\w]*):(.*)/
 
+def is_ignore(ignore_cats, cat)
+	return ignore_cats.include?(cat)
+end
+
+def get_category(cat_h, cat)
+	cat_h.each_pair { |kat, cats|
+		return kat if cats.include?(cat)
+	}
+	nil
+end
+
+unknown={}
 addresses=%w/213.202.233.59 185.53.91.76 70.81.251.194/
 addresses.each { |addr|
 	puts ">>> whois #{addr}"
@@ -34,16 +47,21 @@ addresses.each { |addr|
 		next if line[RE_CAT].nil?
 		cat=$1.strip.downcase
 		val=$2.strip
-		found=nil
-		CAT.each_pair { |kat, cats|
-			next unless cats.include?(cat)
 
-			puts "Info: classify #{cat} as #{kat}: #{val}"
+		next if unknown.keys.include?(cat)
+
+		if is_ignore($ignore, cat)
+			#puts "Debug: classify ignore #{cat}: #{line}"
+			wbc.train(:ignore, line)
+			next
+		end
+		kat = get_category($cat, cat)
+		unless kat.nil?
+			puts "Info: classify #{cat} as #{kat}: #{line}"
 			wbc.train(kat, line)
-			found=kat
-			break
-		}
-		next unless found.nil?
+			next
+		end
+		unknown[cat] = line
 		puts "Warning: #{cat} category not found in input: #{line}"
 	}
 }
@@ -56,6 +74,25 @@ tests.each { |test|
 	cat=wbc.classify test
 	puts "Classified as #{cat}: #{test}"
 }
+
+File.read("whois_sample.txt").each_line { |line|
+	cat = wbc.classify(line)
+	if cat == :cidr || cat.eql?(:cidr.to_s)
+		puts "Info: cidr = #{line}"
+		break
+	end
+}
+
+unless unknown.empty?
+	puts JSON.pretty_generate(unknown)
+	arr='%w/'
+	unknown.keys.each { |key|
+		arr+=key+" "
+	}
+	arr=arr.strip+'/'
+	puts arr
+end
+
 
 #wbc.train_interesting "here are some good words. I hope you love them"
 #wbc.train_uninteresting "here are some bad words, I hate you"
