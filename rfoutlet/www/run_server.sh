3dc7c3fee7be0a47efc2f0323e573ae44c787928
Steeve McCauley <steeve@oneguycoding.com>
2017-11-15 17:28:31 -0500

    added script to run server

---
+++ rfoutlet/www/run_server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ME=$(basename $0 .sh)
+MD=$(cd $(dirname $0); pwd)
+LOG=/var/tmp/rfoutlet_server.$(date +%Y%m%d).log
+
+cd $MD
+echo Logging to $LOG
+nohup ./server.rb >> $LOG 2>&1 &
+
