4752123f875d51cce58713d0d39fc84c7dab0385
Steeve McCauley <steeve@oneguycoding.com>
2020-02-04 09:41:54 -0500

    Simplify setting DBUS_SESSION_BUS_ADDRESS environment variable

--- bin/wallpaper.sh
+++ bin/wallpaper.sh
@@ -2,15 +2,22 @@
 
 if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
 	# grep for gnome-session-binary (pgrep limited to 15 characters)
-	PID=$(pgrep -u $LOGNAME gnome-session)
-	if [ -z "$PID" ]; then
-		echo "Error: gnome-session not running for user $LOGNAME"
-		exit 1
-	fi
+	# ps -u steeve -f | grep gnome-session-binary | grep systemd-service | awk '{printf $2}'
+	#PID=$(pgrep -u $LOGNAME gnome-session)
+	#/run/user/1201/bus
+	BUS=/run/user/$(id -u $LOGNAME)/bus
+	[ ! -S "$BUS" ] && echo "Error: dbus not found at $BUS" && exit 1
+	DBUS_SESSION_BUS_ADDRESS="unix:path=$BUS"
+	export DBUS_SESSION_BUS_ADDRESS
+	#echo $DBUS_SESSION_BUS_ADDRESS
+	#PID=$(ps -u $LOGNAME -f | grep gnome-session-binary | grep systemd-service | awk '{printf $2}')
+	#if [ -z "$PID" ]; then
+	#	echo "Error: gnome-session not running for user $LOGNAME"
+	#	exit 1
+	#fi
 	# cat /proc/${PID}/environ | grep -z DBUS_SESSION_BUS_ADDRESS | tr '\0' '\n'
-	export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ| tr '\0' '\n'|cut -d= -f2-)
-#else
-#	echo $DBUS_SESSION_BUS_ADDRESS
+	#export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ| tr '\0' '\n'|cut -d= -f2-)
+	#echo $DBUS_SESSION_BUS_ADDRESS
 fi
 
 file="file://$(find ${HOME}/Pictures/wallpaper/ -type f | shuf -n1)"
