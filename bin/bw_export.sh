36fb7e6c56eb43b6ab852f90da0805078f10ed15
Steeve McCauley <steeve.mccauley@gmail.com>
2020-12-11 09:18:03 -0500

    Use ${PIPESTATUS[0]} to find errors in authentication

--- bin/bw_export.sh
+++ bin/bw_export.sh
@@ -29,23 +29,32 @@ mkdir -p $BDIR
 cd $BDIR
 [ $? -ne 0 ] && die "Failed to change to backups dir: $BDIR"
 
+info "Working in $BDIR"
+
+email=${BW_EMAIL:-$1}
 bw login --check
 if [ $? -ne 0 ]; then
-	info bw login $1
-	bw login $1 | grep 'export BW_SESSION=' | cut -d'$' -f2 > bw_session
-	[ $? -ne 0 ] && die "Failed to authenticate as username $1"
-	source bw_session
-	echo $BW_SESSION
+	unset BW_SESSION
+	info bw login $email
+	bw login $email | grep 'export BW_SESSION=' | cut -d'$' -f2 > bw_session
+	declare -i res=${PIPESTATUS[0]}
+	[ $res -ne 0 ] && die "Failed to authenticate as username $email"
 	cat bw_session
+	source bw_session
+	[ -z "$BW_SESSION" ] && die "No BW_SESSION found"
+	echo $BW_SESSION
 fi
 
 now=$(date +%Y%m%d_%H%M%S)
-out=${ME}_${now}.json.gpg
+out=$(pwd)/${ME}_${now}.json.gpg
 info bw export --raw --format json
 bw export --raw --format json | gpg -e -o "$out"
-[ $? -ne 0 ] && die "Failed to backup and encrypt output to $out"
+declare -i gpg_res=$?
+declare -i bw_res=${PIPESTATUS[0]}
+[ $bw_res -ne 0 ] && die "Failed to export output to $out"
+[ $gpg_res -ne 0 ] && die "Failed to gpg encrypt bw output to $out"
 
 bw logout
-bw logout
+[ $? -ne 0 ] && die "Failed to logout"
 
 info "backup to $out successful"
