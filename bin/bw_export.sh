d156abf5ea9cc53cbf99e2ec0787d57d816a3b0d
Steeve McCauley <steeve.mccauley@gmail.com>
2020-12-14 13:10:46 -0500

    Detect bw login/unlock/export failure better
    * read data into variable and fail if empty before encrypting
    * don't logout or lock the session
    * print the bw_session file path to continue using

--- bin/bw_export.sh
+++ bin/bw_export.sh
@@ -8,12 +8,20 @@ BDIR=~/Downloads/bw/backups/
 
 umask 0077
 
-log() {
+puts() {
 	echo -e $*
+}
+
+log() {
+	puts $*
 }
 
 info() {
 	log "INFO: $*"
+}
+
+warn() {
+	log "WARN: $*"
 }
 
 err() {
@@ -25,36 +33,80 @@ die() {
 	exit 1
 }
 
+email=${BW_EMAIL:-$1}
+
 mkdir -p $BDIR
 cd $BDIR
 [ $? -ne 0 ] && die "Failed to change to backups dir: $BDIR"
 
 info "Working in $BDIR"
 
-email=${BW_EMAIL:-$1}
+bw_session="$(pwd)/bw_session"
+if [ -z "$BW_SESSION" -a -f ${bw_session} ]; then
+	source ${bw_session}
+	info "BW_SESSION=$BW_SESSION"
+fi
+
+declare -i auth=1
 bw login --check
-if [ $? -ne 0 ]; then
-	unset BW_SESSION
+if [ $? -eq 0 ]; then
+	bw unlock --check
+	if [ $? -eq 0 ]; then
+		auth=0
+	else
+		auth=2
+	fi
+else
+	auth=1
+fi
+
+if [ $auth -eq 1 ]; then
 	info bw login $email
-	bw login $email | grep 'export BW_SESSION=' | cut -d'$' -f2 > bw_session
+	bw login $email > bw_output
+elif [ $auth -eq 2 ]; then
+	info bw unlock
+	bw unlock > bw_output
+else
+	rm -f bw_output
+fi
+
+if [ -f bw_output ]; then
+	unset BW_SESSION
+	info cat $(pwd)/bw_output
+	cat bw_output | grep 'export BW_SESSION=' | cut -d'$' -f2 > ${bw_session}
 	declare -i res=${PIPESTATUS[0]}
 	[ $res -ne 0 ] && die "Failed to authenticate as username $email"
-	cat bw_session
-	source bw_session
-	[ -z "$BW_SESSION" ] && die "No BW_SESSION found"
-	echo $BW_SESSION
+fi
+
+if [ -z "$BW_SESSION" -a -f ${bw_session} ]; then
+	info "source ${bw_session}"
+	source ${bw_session}
+	[ -z "$BW_SESSION" ] && die "BW_SESSION not found"
+	cat ${bw_session}
 fi
 
 now=$(date +%Y%m%d_%H%M%S)
 out=$(pwd)/${ME}_${now}.json.gpg
-info bw export --raw --format json
-bw export --raw --format json | gpg -e -o "$out"
-declare -i gpg_res=$?
-declare -i bw_res=${PIPESTATUS[0]}
-[ $bw_res -ne 0 ] && die "Failed to export output to $out"
-[ $gpg_res -ne 0 ] && die "Failed to gpg encrypt bw output to $out"
+#info bw export --raw --format json
+info "Read vault"
+data=$(bw export --raw --format json)
+[ $? -ne 0 ] && die "Failed to export vault"
+[ -z "$data" ] && die "Export data is empty"
 
-bw logout
-[ $? -ne 0 ] && die "Failed to logout"
+info "Encrypt to $out"
+echo -ne "$data" | gpg -e -o "$out"
+[ $? -ne 0 ] && die "Failed to gpg encrypt bw vault to $out"
 
+unset data
+
+#bw logout
+#[ $? -ne 0 ] && die "Failed to logout"
+
+puts
 info "backup to $out successful"
+puts
+warn "Session is still unlocked"
+puts "\nTo lock: bw lock"
+puts "To logout: bw logout\n"
+puts "BW_SESSION=""${BW_SESSION}"""
+puts "source ${bw_session}"
